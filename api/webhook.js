630c4899b6569caeed30370828bb8a3e9c69158b
proxima812 <mrzuc2015@yandex.ru>
2024-01-14 15:37:07 +0600

    1

--- api/webhook.js
+++ api/webhook.js
@@ -13,34 +13,6 @@ const bot = new TelegramBot(TOKEN)
 
 const botUsername = 'tetris_dusha_bot'
 
-const lastCommandUsage = {}
-
-function canUseCommand(chatId, userId, command) {
-	const currentTime = Date.now()
-	const timeLimit = 180000 // 3 минуты в миллисекундах
-
-	if (!lastCommandUsage[chatId]) {
-		lastCommandUsage[chatId] = {}
-	}
-
-	if (!lastCommandUsage[chatId][userId]) {
-		lastCommandUsage[chatId][userId] = {}
-	}
-
-	if (
-		lastCommandUsage[chatId][userId][command] &&
-		currentTime - lastCommandUsage[chatId][userId][command] < timeLimit
-	) {
-		return false // Пользователь использовал команду менее 3 минут назад
-	}
-
-	// Обновляем время использования команды для пользователя
-	lastCommandUsage[chatId][userId][command] = currentTime
-	return true
-}
-
-
-
 module.exports = async (request, response) => {
 	try {
 		const { body } = request
@@ -69,34 +41,17 @@ module.exports = async (request, response) => {
 				}
 			}
 
-			// if (text === '/stop10' || text === `/stop10@${botUsername}`) {
-			// 	await bot.sendMessage(id, stop10Message, {
-			// 		parse_mode: 'Markdown',
-			// 		disable_web_page_preview: true,
-			// 	})
-			// 	try {
-			// 		await bot.deleteMessage(id, message_id)
-			// 	} catch (error) {
-			// 		console.error('Error deleting message', error.toString())
-			// 	}
-      // }
-      
-   if (text === '/stop10' || text === `/stop10@${botUsername}`) {
-			const userId = body.message.from.id // ID пользователя, отправившего команду
-
-			if (canUseCommand(id, userId, 'stop10')) {
+			if (text === '/stop10' || text === `/stop10@${botUsername}`) {
 				await bot.sendMessage(id, stop10Message, {
 					parse_mode: 'Markdown',
 					disable_web_page_preview: true,
 				})
-			} else {
-				// Отправить сообщение о блокировке команды из-за анти-спама
-				await bot.sendMessage(
-					id,
-					'Пожалуйста, подождите перед повторным использованием команды.',
-				)
+				try {
+					await bot.deleteMessage(id, message_id)
+				} catch (error) {
+					console.error('Error deleting message', error.toString())
+				}
 			}
-		}
 
 			if (text === '/stop11' || text === `/stop11@${botUsername}`) {
 				await bot.sendMessage(id, stop11Message, { parse_mode: 'Markdown' })
